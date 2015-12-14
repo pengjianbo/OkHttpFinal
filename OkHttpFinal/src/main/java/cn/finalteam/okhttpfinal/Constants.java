279e47cb04a0131b5fdcbff8ed9023b5fd711468
彭建波 <pengjianbosoft@gmail.com>
2015-12-14 18:47:20 +0800

    去掉gson

--- OkHttpFinal/src/main/java/cn/finalteam/okhttpfinal/Constants.java
+++ OkHttpFinal/src/main/java/cn/finalteam/okhttpfinal/Constants.java
@@ -16,17 +16,13 @@
 
 package cn.finalteam.okhttpfinal;
 
-import java.io.File;
-
 /**
  * Desction:
  * Author:pengjianbo
  * Date:15/9/21 上午9:54
  */
-public class Constants {
+class Constants {
     protected static boolean DEBUG = BuildConfig.DEBUG;
-    //下载管理目标文件夹
-    public static final String DM_TARGET_FOLDER = File.separator + "download" + File.separator;
     //Http请求超时时间
     public static final int REQ_TIMEOUT = 30000;
 }
